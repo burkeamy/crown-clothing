1b692f04c1586573f0f40734b0d44cdc27be0731
Amy Burke <burkeamy30@gmail.com>
2021-02-25 19:15:30 -0500

    added router for navigation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import HomePage from './pages/homepage/homepage.component';
+import React from 'react';
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
+
+import HomePage from './pages/homepage/homepage.component';
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+);
 
 function App() {
   return (
-    <div className="App">
-      <HomePage/>
+    <div>
+      <Switch>
+        <Route exact path='/' component={ HomePage }/>
+        <Route path='/hats' component={HatsPage} />
+      </Switch>
     </div>
   );
 }
