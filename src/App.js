53b97b451ee8716755e6118258aa5953da619b6e
Amy Burke <burkeamy30@gmail.com>
2021-02-27 09:46:50 -0500

    adding shop page, collection preview components

--- src/App.js
+++ src/App.js
@@ -3,19 +3,16 @@ import './App.css';
 import { Switch, Route } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.components'
 
-const HatsPage = () => (
-  <div>
-    <h1>Hats Page</h1>
-  </div>
-);
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={ HomePage }/>
-        <Route path='/hats' component={HatsPage} />
+        <Route path='/shop' component={ShopPage}/>
+      
       </Switch>
     </div>
   );
