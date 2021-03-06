007bdf8a6daede25148f680cecb51a7b5ff1e8d0
Amy Burke <burkeamy30@gmail.com>
2021-03-06 10:27:33 -0500

    added sign in section. It is not connectd to a database yet. that is the next step

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { Switch, Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.components';
 import Header from './components/header/header.component';
-
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 function App() {
   return (
@@ -13,7 +13,8 @@ function App() {
       <Header />
       <Switch>
         <Route exact path='/' component={ HomePage }/>
-        <Route path='/shop' component={ShopPage}/>
+        <Route path='/shop' component={ ShopPage }/>
+        <Route path='/signin' component={ SignInAndSignUpPage }/>
       
       </Switch>
     </div>
