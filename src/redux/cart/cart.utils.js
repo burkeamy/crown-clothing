96444d45af5268b996b829cb5808df9619184e8f
unknown <burkeamy30@gmail.com>
2021-03-17 19:27:59 -0400

    items show up in the shopping bag using a cart-item component

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -5,7 +5,7 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
         if (existingCartItem) {
             return cartItems.map(cartItem => 
                 cartItem.id === cartItemToAdd.id
-                    ? {...cartItem, quantitiy: cartItem.quantity + 1}
+                    ? {...cartItem, quantity: cartItem.quantity + 1}
                     : cartItem
                 )
         }
